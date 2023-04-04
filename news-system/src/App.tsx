37db682f0eadbece203ece2845854681371056c3
oneShot <e.beautrader@outlook.com>
2023-04-05 00:42:55 +0800

    Revert "new-system/App add test"
    This reverts commit 3cd16dc21795f247810a3a1198eb2b2fc780630c.

--- news-system/src/App.tsx
+++ news-system/src/App.tsx
@@ -4,19 +4,19 @@
  * @LastEditors: error: error: git config user.name & please set dead value or install git && error: git config user.email & please set dead value or install git & please set dead value or install git
  * @LastEditTime: 2023-03-22 20:40:02
  * @FilePath: \Workspace\newsSystem\src\App.tsx
- * @Description:
- * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
+ * @Description: 
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved. 
  */
-import React from "react";
-import { HashRouter } from "react-router-dom";
-import IndexRouter from "./routers/IndexRouter";
+import React from 'react'
+import { HashRouter } from 'react-router-dom'
+import IndexRouter from './routers/IndexRouter'
 
 const App = () => {
   return (
     <HashRouter>
       <IndexRouter></IndexRouter>
     </HashRouter>
-  );
-};
-//test
-export default App;
+  )
+}
+
+export default App
