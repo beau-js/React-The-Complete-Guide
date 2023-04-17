88ee6be493b7c0f3ca286e863f8dc4371cfbbd72
Beau <pg.beau@outlook.com>
2023-04-17 16:38:09 +0800

    teacher correct code

--- 20-React-Router/12-adv-starting-project/frontend/src/components/EventForm.jsx
+++ 20-React-Router/12-adv-starting-project/frontend/src/components/EventForm.jsx
@@ -2,20 +2,40 @@
  * @Author: Beau pg.beau@outlook.com
  * @Date: 2023-01-16 15:49:04
  * @LastEditors: Beau pg.beau@outlook.com
- * @LastEditTime: 2023-04-16 21:54:02
+ * @LastEditTime: 2023-04-17 15:28:20
  * @FilePath: \workspace\React-The-Complete-Guide\20-React-Router\12-adv-starting-project\frontend\src\components\EventForm.jsx
  * @Description:
  *
  * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
  */
-import { Form } from 'react-router-dom';
+import {
+  Form,
+  useActionData,
+  useNavigate,
+  useNavigation,
+} from 'react-router-dom';
 import classes from './EventForm.module.css';
 
 function EventForm({ method, event }) {
-  function cancelHandler() {}
+  const data = useActionData();
+  const navigate = useNavigate();
+  const navigation = useNavigation();
+
+  const isSubmitting = navigation.state === 'submitting';
+
+  function cancelHandler() {
+    navigate('..');
+  }
 
   return (
     <Form method="post" className={classes.form}>
+      {data && data.errors && (
+        <ul>
+          {Object.values(data.errors).map((error, index) => (
+            <li key={error}>{error}</li>
+          ))}
+        </ul>
+      )}
       <p>
         <label htmlFor="title">
           Title
@@ -65,10 +85,12 @@ function EventForm({ method, event }) {
         </label>
       </p>
       <div className={classes.actions}>
-        <button type="button" onClick={cancelHandler}>
+        <button type="button" onClick={cancelHandler} disabled={isSubmitting}>
           Cancel
         </button>
-        <button type="submit">Save</button>
+        <button type="submit" disabled={isSubmitting}>
+          {isSubmitting ? 'Submitting...' : 'Save'}
+        </button>
       </div>
     </Form>
   );
--- 20-React-Router/12-adv-starting-project/frontend/src/pages/NewEvent.js
+++ 20-React-Router/12-adv-starting-project/frontend/src/pages/NewEvent.js
@@ -1,14 +1,5 @@
-/*
- * @Author: Beau pg.beau@outlook.com
- * @Date: 2023-04-13 20:55:20
- * @LastEditors: Beau pg.beau@outlook.com
- * @LastEditTime: 2023-04-16 21:54:37
- * @FilePath: \workspace\React-The-Complete-Guide\20-React-Router\12-adv-starting-project\frontend\src\views\NewEventPage.jsx
- * @Description:
- *
- * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
- */
 import { json, redirect } from 'react-router-dom';
+
 import EventForm from '../components/EventForm';
 
 function NewEventPage() {
@@ -27,14 +18,16 @@ export async function action({ request, params }) {
     description: data.get('description'),
   };
 
-  const response = fetch('http://loaclhost:8080/events', {
+  const response = await fetch('http://localhost:8080/events', {
     method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
+    headers: {
+      'Content-Type': 'application/json',
+    },
     body: JSON.stringify(eventData),
   });
 
   if (!response.ok) {
-    throw json({ message: 'Could not save event' }, { status: 500 });
+    throw json({ message: 'Could not save event.' }, { status: 500 });
   }
 
   return redirect('/events');
