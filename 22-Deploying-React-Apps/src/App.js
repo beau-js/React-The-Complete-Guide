857ebf79667902f170be7007460c90cd9fc6c818
Beau <pg.beau@outlook.com>
2023-04-20 21:09:12 +0800

    22-Deploying React Apps lazyLoad COMPLETE

--- 22-Deploying-React-Apps/src/App.js
+++ 22-Deploying-React-Apps/src/App.js
@@ -1,10 +1,23 @@
+/*
+ * @Author: Beau pg.beau@outlook.com
+ * @Date: 2022-11-11 13:34:54
+ * @LastEditors: Beau pg.beau@outlook.com
+ * @LastEditTime: 2023-04-20 16:46:04
+ * @FilePath: \workspace\React-The-Complete-Guide\22-Deploying-React-Apps\src\App.js
+ * @Description:
+ *
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
+ */
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
 
-import BlogPage, { loader as postsLoader } from './pages/Blog';
+// import BlogPage, { loader as postsLoader } from './pages/Blog';
 import HomePage from './pages/Home';
-import PostPage, { loader as postLoader } from './pages/Post';
+// import PostPage, { loader as postLoader } from './pages/Post';
 import RootLayout from './pages/Root';
 
+const BlogPage = lazy(() => import('./pages/Blog'));
+const PostPage = lazy(() => import('./pages/Post'));
 const router = createBrowserRouter([
   {
     path: '/',
@@ -17,8 +30,29 @@ const router = createBrowserRouter([
       {
         path: 'posts',
         children: [
-          { index: true, element: <BlogPage />, loader: postsLoader },
-          { path: ':id', element: <PostPage />, loader: postLoader },
+          {
+            index: true,
+            element: (
+              <Suspense fallback={<p>Loading...</p>}>
+                <BlogPage />
+              </Suspense>
+            ),
+            loader: () =>
+              import('./pages/Blog').then((module) => module.loader()),
+          },
+
+          {
+            path: ':id',
+            element: (
+              <Suspense fallback={<p>Loading...</p>}>
+                <PostPage />
+              </Suspense>
+            ),
+            loader: ({ params }) =>
+              import('./pages/Post').then((module) =>
+                module.loader({ params })
+              ),
+          },
         ],
       },
     ],
--- 22-Deploying-React-Apps/src/pages/Post.js
+++ 22-Deploying-React-Apps/src/pages/Post.js
@@ -12,5 +12,5 @@ export default PostPage;
 
 export function loader({ params }) {
   const postId = params.id;
-  return fetch('https://jsonplaceholder.typicode.com/posts/' + postId);
+  return fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`);
 }
