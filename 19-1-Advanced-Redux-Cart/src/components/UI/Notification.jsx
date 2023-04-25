3ac10d011a28bb5e5ba0656d7c77176d70ed934b
Beau <pg.beau@outlook.com>
2023-04-25 14:14:35 +0800

    add README.md

--- 19-1-Advanced-Redux-Cart/src/components/UI/Notification.jsx
+++ 19-1-Advanced-Redux-Cart/src/components/UI/Notification.jsx
@@ -1,23 +1,33 @@
-import classes from "./Notification.module.css"
+/*
+ * @Author: Beau pg.beau@outlook.com
+ * @Date: 2023-04-12 03:14:50
+ * @LastEditors: Beau pg.beau@outlook.com
+ * @LastEditTime: 2023-04-25 14:14:09
+ * @FilePath: \workspace\React-The-Complete-Guide\19-1-Advanced-Redux-Cart\src\components\UI\Notification.jsx
+ * @Description:
+ *
+ * Copyright (c) 2023 by ${git_name_email}, All Rights Reserved.
+ */
+import classes from './Notification.module.css';
 
 const Notification = (props) => {
-  let specialClasses = ""
+  let specialClasses = '';
 
-  if (props.status === "error") {
-    specialClasses = classes.error
+  if (props.status === 'error') {
+    specialClasses = classes.error;
   }
-  if (props.status === "success") {
-    specialClasses = classes.success
+  if (props.status === 'success') {
+    specialClasses = classes.success;
   }
 
-  const cssClasses = `${classes.notification} ${specialClasses}`
+  const cssClasses = `${classes.notification} ${specialClasses}`;
 
   return (
     <section className={cssClasses}>
       <h2>{props.title}</h2>
       <p>{props.message}</p>
     </section>
-  )
-}
+  );
+};
 
-export default Notification
+export default Notification;
